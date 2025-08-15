5adbe088273a624be37c69e6c3a7aa9be7401318
ocegik <147722344+ocegik@users.noreply.github.com>
2025-08-15 19:17:49 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.VisualBasic;
+
+
+public class Extension
+{
+    public string[] Images { get; set; } = Array.Empty<string>();
+    public string[] Videos { get; set; } = Array.Empty<string>();
+    public string[] Documents { get; set; } = Array.Empty<string>();
+    public string[] Audio { get; set; } = Array.Empty<string>();
+}
+
+class FileOrganizer
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("=== File Organizer ===");
+
+        Console.Write("Enter the full path of folder to organize: ");
+        string targetFolder = Console.ReadLine() ?? "";
+
+        if (!Directory.Exists(targetFolder))
+        {
+            Console.WriteLine("The Folder doesn't exist at given path.");
+            return;
+        }
+        string extensionListPath = "FileExtensionsList.json";
+        Dictionary<string, string[]> categories;
+
+        try
+        {
+            categories = LoadCategoriesFromJson(extensionListPath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unable to load Json file: {ex.Message} ");
+            return;
+        }
+
+
+        string logFilePath = Path.Combine(targetFolder, "organizer_log.txt");
+        using StreamWriter LogWriter = new StreamWriter(logFilePath, append: true);
+
+        int movedCount = 0;
+
+        foreach (string file in Directory.GetFiles(targetFolder))
+        {
+            string extension = Path.GetExtension(file).ToLower();
+            string fileName = Path.GetFileName(file);
+
+            string categoryFolder = "Others";
+
+            foreach (var category in categories)
+            {
+                if (Array.Exists(category.Value, ext => ext == extension))
+                {
+                    categoryFolder = category.Key;
+                    break;
+                }
+            }
+            string destinationFolder = Path.Combine(targetFolder, categoryFolder);
+
+            if (!Directory.Exists(destinationFolder))
+                Directory.CreateDirectory(destinationFolder);
+
+            string destinationPath = Path.Combine(destinationFolder, fileName);
+
+            try
+            {
+                File.Move(file, destinationPath);
+                LogWriter.WriteLine($"{DateTime.Now}: Moved {fileName} → {categoryFolder}");
+                movedCount++;
+            }
+            catch (Exception ex)
+            {
+                LogWriter.WriteLine($"{DateTime.Now}: Failed to move {fileName} → {ex.Message}");
+            }
+
+        }
+        Console.WriteLine($"✅ Done! Moved {movedCount} files. Log saved to organizer_log.txt");
+
+    }
+    static Dictionary<string, string[]> LoadCategoriesFromJson(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"Json File not found: {filePath}");
+        }
+
+        string jsonString = File.ReadAllText(filePath);
+        var fileExtensions = JsonSerializer.Deserialize<Extension>(jsonString);
+
+        if (fileExtensions == null)
+        {
+            throw new InvalidOperationException("Failed to deserialize JSON file");
+        }
+        return new Dictionary<string, string[]>
+        {
+            { "Images", fileExtensions.Images },
+            { "Videos", fileExtensions.Videos },
+            { "Documents", fileExtensions.Documents },
+            { "Audio", fileExtensions.Audio }
+        };
+    }
+}
---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program
+{
+    static void Extra(string[] args)
+    {
+        Console.WriteLine("working");
+    }
+}
