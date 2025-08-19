0070f22e2fa37d3cb28c49cb79b65d2810060200
ocegik <147722344+ocegik@users.noreply.github.com>
2025-08-19 12:27:08 +0530

    Refactor JSON loading logic into LoadJsonFile class

---
+++ LoadJsonFile.cs
@@ -0,0 +1,19 @@
+using System.Text.Json;
+
+namespace FileOrganizer
+{
+    public class LoadJsonFile
+    {
+        public static Dictionary<string, string[]> LoadJson(string extensionListPath = "FileExtensionsList.json")
+        {
+            if (!File.Exists(extensionListPath))
+            {
+                throw new FileNotFoundException($"Json File not found: {extensionListPath}");
+            }
+            string jsonString = File.ReadAllText(extensionListPath);
+
+             return JsonSerializer.Deserialize<Dictionary<string, string[]>>(jsonString)
+               ?? throw new InvalidOperationException("Failed to deserialize JSON file");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -21,32 +21,16 @@ class Program
         }
 
         string extensionListPath = "FileExtensionsList.json";
-        Dictionary<string, string[]> categories;
 
         try
         {
-            categories = LoadCategoriesFromJson(extensionListPath);
+            var categories = LoadJsonFile.LoadJson(extensionListPath);
+            OrganizingOptions.Options(targetFolder, categories);
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Unable to load Json file: {ex.Message}");
             return;
-        }
-
-        OrganizingOptions.Options(targetFolder, categories);
-        
-
-        static Dictionary<string, string[]> LoadCategoriesFromJson(string filePath)
-        {
-            if (!File.Exists(filePath))
-            {
-                throw new FileNotFoundException($"Json File not found: {filePath}");
-            }
-
-            string jsonString = File.ReadAllText(filePath);
-
-            return JsonSerializer.Deserialize<Dictionary<string, string[]>>(jsonString)
-               ?? throw new InvalidOperationException("Failed to deserialize JSON file");
         }
     }
 }
